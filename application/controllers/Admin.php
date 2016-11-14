07e6bf65be695ecd1e1fea10bd7ce8215d56d38a
gurvirjohal <j@j-PC>
2016-11-13 16:30:32 -0800

    issues from PartA fixed PartB - Steps 7x-8 done Signed-off-by:gurvirjohal <j@j-PC>

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,21 +1,122 @@
 <?php
 
 class Admin extends Application {
-    function __construct() {
-        parent::__construct();
-    }
-    
-    public function index(){
-        $origin = $_SERVER['HTTP_REFERER'];
-        $role = $this->session->userdata('userrole');
-        if ($role != 'admin') {
-            $this->data['content'] = 'Access Denied';
-            $this->render();
-            return;
-        }
+            function __construct() {
+                parent::__construct();
+                $this->load->helper('formfields');
+                $this->error_messages = array();
+            }
 
-        $this->data['pagebody' ] ='mtce';
-        $this->data['items'] = $this->menu->all();
-        $this->render();
-    }
+            public function index(){
+                $role = $this->session->userdata('userrole');
+                if ($role != 'admin') {
+                    $this->data['content'] = 'Access Denied';
+                    $this->render();
+                    return;
+                }
+
+                $this->data['pagebody' ] ='mtce';
+                $this->data['items'] = $this->menu->all();
+                $this->render();
+            }
+    
+            function edit($id=null) {
+            // try the session first
+            $key = $this->session->userdata('key');
+            $record = $this->session->userdata('record');
+
+            // if not there, get them from the database
+            if (empty($record)) {
+                    $record = $this->menu->get($id);
+                    $key = $id;
+                    $this->session->set_userdata('key',$id);
+                    $this->session->set_userdata('record',$record);
+            }
+
+            // build the form fields
+            $this->data['fid'] = makeTextField('Menu code', 'id', $record->id);
+            $this->data['fname'] = makeTextField('Item name', 'name', $record->name);
+            $this->data['fdescription'] = makeTextArea('Description', 'description', $record->description);
+            $this->data['fprice'] = makeTextField('Price, each', 'price', $record->price);
+            $this->data['fpicture'] = makeTextField('Item image', 'picture', $record->picture);
+            
+            // categories combobox
+            $cats = $this->categories->all(); // get an array of category objects
+            foreach($cats as $code => $category) // make it into an associative array
+                $codes[$category->id] = $category->name;
+            $this->data['fcategory'] = makeCombobox('Category', 'category', $record->category,$codes);
+
+            $this->data['zsubmit'] = makeSubmitButton('Save', 'Submit changes');    // Submit btn
+            
+            // show the editing form
+            $this->data['pagebody'] = "mtce-edit";
+            $this->show_any_errors();
+            $this->render();
+        }
+        
+        function save() {
+            // try the session first
+            $key = $this->session->userdata('key');
+            $record = $this->session->userdata('record');
+
+            // if not there, nothing is in progress
+            if (empty($record)) {
+                $this->index();
+                return;
+            }
+            
+            // update our data transfer object
+            $incoming = $this->input->post();
+            foreach(get_object_vars($record) as $index => $value)
+                if (isset($incoming[$index]))
+                    $record->$index = $incoming[$index];
+            $this->session->set_userdata('record',$record);
+            // validate
+            $this->load->library('form_validation');
+            $this->form_validation->set_rules($this->menu->rules());
+            if ($this->form_validation->run() != TRUE)
+                    $this->error_messages = $this->form_validation->error_array();
+
+            // check menu code for additions
+            if ($key == null) 
+                    if ($this->menu->exists($record->id))
+                            $this->error_messages[] = 'Duplicate key adding new menu item';
+            if (! $this->categories->exists($record->category))
+                    $this->error_messages[] = 'Invalid category code: ' . $record->category;
+            
+            // save or not
+            if (! empty($this->error_messages)) {
+                    $this->edit();
+                    return;
+            }
+
+            // update our table, finally!
+            if ($key == null)
+                    $this->menu->add($record);
+            else
+                    $this->menu->update($record);
+            // and redisplay the list
+            $this->index(); 
+        }
+        
+        function cancel() {
+            $this->session->unset_userdata('key');
+            $this->session->unset_userdata('record');
+            $this->index();
+        }
+        
+        function show_any_errors() {
+            $result = '';
+            if (empty($this->error_messages)) {
+                $this->data['error_messages'] = '';
+                return;
+            }
+            // add the error messages to a single string with breaks
+            foreach($this->error_messages as $onemessage)
+                $result .= $onemessage . '<br/>';
+            // and wrap these per our view fragment
+            $this->data['error_messages'] = $this->parser->parse('mtce-errors',
+                    ['error_messages' => $result], true);
+        }
+    
 }
--- application/controllers/Toggle.php
+++ application/controllers/Toggle.php
@@ -12,51 +12,12 @@
  * @author j
  */
 class Toggle extends Application {
+    
 	public function index()	{
-		$origin = $_SERVER['HTTP_REFERER'];
 		$role = $this->session->userdata('userrole');
 		if ($role == 'user') $role = 'admin';
 		else $role = 'user';
 		$this->session->set_userdata('userrole', $role);
 		redirect($origin);
 	}
-        
-        function edit($id=null) {
-            // try the session first
-            $key = $this->session->userdata('key');
-            $record = $this->session->userdata('record');
-
-            // if not there, get them from the database
-            if (empty($key)) {
-                    $record = $this->menu->get($id);
-                    $key = $id;
-                    $this->session->set_userdata('key',$id);
-                    $this->session->set_userdata('record',$record);
-            }
-
-            // build the form fields
-            $this->data['fid'] = makeTextField('Menu code', 'id', $record->id);
-            $this->data['fname'] = makeTextField('Item name', 'name', $record->name);
-            $this->data['fdescription'] = makeTextArea('Description', 'description', $record->description);
-            $this->data['fprice'] = makeTextField('Price, each', 'price', $record->price);
-            $this->data['fpicture'] = makeTextField('Item image', 'picture', $record->picture);
-            
-            // categories combobox
-            $cats = $this->categories->all(); // get an array of category objects
-            foreach($cats as $code => $category) // make it into an associative array
-                $codes[$code] = $category->name;
-            $this->data['fcategory'] = makeCombobox('Category', 'category', $record->category,$codes);
-
-            $this->data['zsubmit'] = makeSubmitButton('Save', 'Submit changes');    // Submit btn
-            
-            // show the editing form
-            $this->data['pagebody'] = "mtce-edit";
-            $this->render();
-        }
-        
-        function cancel() {
-            $this->session->unset_userdata('key');
-            $this->session->unset_userdata('record');
-            $this->index();
-        }
 }
